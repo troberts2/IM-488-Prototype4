93d56182d8967896b98770d9cd1440f8317c4739
rswansonbradley <raswanson@mail.bradley.edu>
2024-04-11 23:40:54 -0500

    TMPImport

--- Assets/Scripts/Managers/Universal/UniversalManagers.cs
+++ Assets/Scripts/Managers/Universal/UniversalManagers.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class UniversalManagers : MonoBehaviour
 {
     public static UniversalManagers Instance;
-    /*[SerializeField] private SceneLoadManager _sceneLoadManager;
-    [SerializeField] private AudioManager _audioManager;*/
+    [SerializeField] private SceneLoadManager _sceneLoadManager;
+    //[SerializeField] private AudioManager _audioManager;
 
     // Start is called before the first frame update
     void Awake()
@@ -27,11 +27,11 @@ public class UniversalManagers : MonoBehaviour
     }
 
     #region Get Managers
-    /*public SceneLoadManager GetSceneLoadManager()
+    public SceneLoadManager GetSceneLoadManager()
     {
         return _sceneLoadManager;
     }
-    public AudioManager GetAudioManager()
+    /*public AudioManager GetAudioManager()
     {
         return _audioManager;
     }*/
