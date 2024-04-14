7066bf1f876792796949865ef5e0fe0a53329f92
Nabil Tagba <nabiltagba@gmail.com>
2024-04-13 22:28:45 -0500

    conveyor belt

---
+++ Assets/Scripts/Conveyor.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class Conveyor : MonoBehaviour
+{
+    [SerializeField] Transform targetPos;
+    [SerializeField] float speed;
+
+   public GameObject currentTrash;
+    private bool isThereTrashOnConveyor = false;
+
+    [SerializeField] GameObject trashHandleButtons;
+
+    private void Update()
+    {
+        if (isThereTrashOnConveyor)
+        {
+            
+            if (currentTrash != null && currentTrash.transform.position != targetPos.position)
+            {
+                currentTrash.transform.position = Vector3.MoveTowards(currentTrash.transform.position, targetPos.position, speed * Time.deltaTime );
+                trashHandleButtons.SetActive(false);
+            }
+
+            if (currentTrash != null && currentTrash.transform.position == targetPos.position)
+            {
+                //enable trash handle buttons
+                trashHandleButtons.SetActive(true);
+
+            }
+        }
+
+       
+      
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Trash")
+        {
+            currentTrash = collision.gameObject;
+            currentTrash.GetComponent<Rigidbody>().useGravity = false;
+            isThereTrashOnConveyor = true;
+        }
+    }
+
+    
+    void OnCollisionExit(Collision collision)
+    {
+        // was picked up or something
+        if (collision.gameObject == currentTrash)
+        {
+            currentTrash.GetComponent<Rigidbody>().useGravity = true;
+            isThereTrashOnConveyor = false;
+            currentTrash = null;
+            gameObject.GetComponentInChildren<TrashSpawner>().isReadyForNextItem = true;
+            print("exit");
+
+        }
+    }
+}
